64cae846a730b6bbfbe68746ff1ca1f7ee7dda68
chocoalano <alangentina95@gmail.com>
2024-10-13 09:38:58 +0700

    new update 13/10/2024

---
+++ start/routes_app/auth.ts
@@ -0,0 +1,21 @@
+import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+import { apiThrottle } from '../limiter.js'
+router.post('login', '#controllers/auth_controller.login').use(apiThrottle)
+router
+  .group(() => {
+    router.get('logout', '#controllers/auth_controller.logout')
+    router.get('user-auth', '#controllers/auth_controller.user_auth')
+    router.put('user-update/:id', '#controllers/auth_controller.update')
+    router.post('user-update/:datatype', '#controllers/auth_controller.updateMobile')
+    router.get(
+      'users-kelengkapan-form',
+      '#controllers/masterdata/users_controller.kelengkapan_form'
+    )
+  })
+  .use(
+    middleware.auth({
+      guards: ['api'],
+    })
+  )
+  .use(apiThrottle)
