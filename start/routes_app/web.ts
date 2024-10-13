4b0458cc68b28ddb2ef2644f75780da076386fab
chocoalano <alangentina95@gmail.com>
2024-10-13 10:08:27 +0700

    update routes

--- start/routes_app/web.ts
+++ start/routes_app/web.ts
@@ -1,31 +1,58 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
 import { apiThrottle } from '../limiter.js'
+const NotificationController = () =>
+  import('#controllers/web_sevices/configs/notification_controller')
+const KoreksiAbsenController = () =>
+  import('#controllers/web_sevices/administrations/koreksi_absen_controller')
+const CutiController = () => import('#controllers/web_sevices/administrations/cuti_controller')
+const PerubahanShiftsController = () =>
+  import('#controllers/web_sevices/administrations/perubahan_shifts_controller')
+const WorkOvertimeController = () =>
+  import('#controllers/mobile_services/administrations/work_overtime_controller')
+const ScheduleGroupAbsensController = () =>
+  import('#controllers/web_sevices/administrations/schedule_group_absens_controller')
+const CompaniesController = () => import('#controllers/web_sevices/configs/companies_controller')
+const BugController = () => import('#controllers/web_sevices/configs/bug_controller')
+const BranchesController = () => import('#controllers/web_sevices/configs/branches_controller')
+const LevelsController = () => import('#controllers/web_sevices/configs/levels_controller')
+const OrganizationsController = () =>
+  import('#controllers/web_sevices/configs/organizations_controller')
+const JobsController = () => import('#controllers/web_sevices/configs/jobs_controller')
+const TimeConfigsController = () =>
+  import('#controllers/web_sevices/configs/time_configs_controller')
+const GroupAbsensController = () =>
+  import('#controllers/web_sevices/administrations/group_absens_controller')
+const DashboardController = () => import('#controllers/web_sevices/dashboard_controller')
+const RoleController = () => import('#controllers/web_sevices/configs/role_controller')
+const UsersController = () => import('#controllers/web_sevices/masterdata/users_controller')
+const AnnouncementController = () =>
+  import('#controllers/web_sevices/masterdata/announcement_controller')
+const AttendancesController = () =>
+  import('#controllers/web_sevices/administrations/attendances_controller')
+
 router
   .group(() => {
-    router.get('dashboard', '#controllers/dashboard_controller.index')
-    router.resource('role', '#controllers/configs/role_controller')
-    router.resource('users', '#controllers/masterdata/users_controller')
-    router.resource('announcement', '#controllers/masterdata/announcement_controller')
-    router.resource('attendance', '#controllers/administrations/attendances_controller')
-    router.resource('company', '#controllers/configs/companies_controller')
-    router.resource('bug', '#controllers/configs/bug_controller')
-    router.resource('branch', '#controllers/configs/branches_controller')
-    router.resource('level', '#controllers/configs/levels_controller')
-    router.resource('dept', '#controllers/configs/organizations_controller')
-    router.resource('job', '#controllers/configs/jobs_controller')
-    router.resource('time', '#controllers/configs/time_configs_controller')
-    router.resource('attendance-group', '#controllers/administrations/group_absens_controller')
-    router.resource(
-      'attendance-group-schedule',
-      '#controllers/administrations/schedule_group_absens_controller'
-    )
-    router.resource('work-overtime', '#controllers/administrations/work_overtime_controller')
-    router.resource('cuti', '#controllers/administrations/cuti_controller')
-    router.resource('perubahan-shift', '#controllers/administrations/perubahan_shifts_controller')
-    router.resource('koreksi-absen', '#controllers/administrations/koreksi_absen_controller')
-    router.get('time-all', '#controllers/configs/time_configs_controller.showAll')
-    router.resource('notification', '#controllers/configs/notification_controller')
+    router.get('dashboard', [DashboardController, 'index'])
+    router.resource('role', RoleController)
+    router.resource('users', UsersController)
+    router.resource('announcement', AnnouncementController)
+    router.resource('attendance', AttendancesController)
+    router.resource('company', CompaniesController)
+    router.resource('bug', BugController)
+    router.resource('branch', BranchesController)
+    router.resource('level', LevelsController)
+    router.resource('dept', OrganizationsController)
+    router.resource('job', JobsController)
+    router.resource('time', TimeConfigsController)
+    router.resource('attendance-group', GroupAbsensController)
+    router.resource('attendance-group-schedule', ScheduleGroupAbsensController)
+    router.resource('work-overtime', WorkOvertimeController)
+    router.resource('cuti', CutiController)
+    router.resource('perubahan-shift', PerubahanShiftsController)
+    router.resource('koreksi-absen', KoreksiAbsenController)
+    router.get('time-all', [TimeConfigsController, 'showAll'])
+    router.resource('notification', NotificationController)
   })
   .use(middleware.auth({ guards: ['api'] }))
   .prefix('web')
